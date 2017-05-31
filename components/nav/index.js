026a49912bb595e601757044969ebd8747e80e62
Oliver Turner <Oliver.Turner@Ticketmaster.co.uk>
2017-05-31 18:38:46 +0100

    Extracted query

--- components/nav/index.js
+++ components/nav/index.js
@@ -3,34 +3,12 @@
 import {gql, graphql} from 'react-apollo'
 import {mergeDeepWithKey, concat} from 'ramda'
 
+import starredRepositories from './query.graphql'
 import Layout from './layout'
 
 const POSTS_PER_PAGE = 100
 
 const mergeRepos = (k, l, r) => k === 'edges' ? concat(l, r) : r
-
-const starredRepositories = gql`
-  query starredRepositories($first: Int!, $after: String) {
-    viewer {
-      starredRepositories(first: $first, after: $after) {
-        totalCount
-        edges {
-          node {
-            id
-            name
-            url
-            description
-            pushedAt
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-        }
-      }
-    }
-  }
-`
 
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (PostList)
